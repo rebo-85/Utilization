5eb9c626ea4de97656988576d6b8613159548914
ReBo <restrainboy@gmail.com>
2025-01-12 08:00:45 +0800

    reset

--- ReBo - Utilization BP/scripts/RedstoneDustPlus.js
+++
@@ -1,9 +0,0 @@
-import { display } from "./ReBo/utils";
-import { afterEvents } from "./ReBo/constants";
-
-afterEvents.playerPlaceBlock.subscribe((event) => {
-  const block = event.block;
-  const asd = block.permutation.getAllStates();
-
-  display(asd);
-});
